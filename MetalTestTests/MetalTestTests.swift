39a7f75d7fb97be64132abb545974bc9190ef8f5
Rob Gilbert <fox015@yahoo.com>
2015-11-09 22:07:56 +1000

    Initial Commit

---
+++ MetalTestTests/MetalTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  MetalTestTests.swift
+//  MetalTestTests
+//
+//  Created by Rob Gilbert on 9/11/2015.
+//  Copyright © 2015 Ninety Four. All rights reserved.
+//
+
+import XCTest
+@testable import MetalTest
+
+class MetalTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
